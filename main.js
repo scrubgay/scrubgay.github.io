a98b32e467b0464330c4807384fc7f67f85b51c0
Renz Torres <wr3ns@protonmail.com>
2022-07-01 15:03:41 -0400

    Upload of new website

--- main.js
+++ main.js
@@ -1,45 +1,3 @@
-// to operate on projects page
-
-// adds contents to TOC and adds a refer back link to table of contents
-function populateTOC() {
-    const contents = document.querySelectorAll("article > section:not(#toc)");
-    const table = document.querySelector("article > section#toc > ul");
-
-    /*                        <div class="prominent toc-return">
-                            <a href="#toc">Return to top...</a>
-                        </div> */
-    // referback
-
-    for (let content of contents) {
-        // add to TOC
-        let contentHeader = content.querySelector("h2");;
-        if (contentHeader !== null) {
-            let contentName = contentHeader.textContent;
-            let id = content.id;
-
-            let tableContent = document.createElement("a");
-            tableContent.href = "#" + id;
-            tableContent.textContent = contentName;
-
-            let tableItem = document.createElement("li");
-            tableItem.appendChild(tableContent);
-
-            table.appendChild(tableItem);
-        }
-
-        // add refer back link
-        let tocReturn = document.createElement("a");
-        tocReturn.href = "#toc";
-        tocReturn.textContent = "Return to top";
-        let tocReturnDiv = document.createElement("div");
-        tocReturnDiv.classList.add("prominent", "toc-return");
-        tocReturnDiv.appendChild(tocReturn);
-
-        content.appendChild(tocReturnDiv);
-    }
-    return;
-}
-
 function createCollapses() {
     const collapsibles = document.querySelectorAll(".collapsible");
     for (let collapsible of collapsibles) {
@@ -57,7 +15,6 @@ function createExpandAll() {
     const expandAllButton = document.querySelector(".toggleCollapseAll");
     const collapsibles = document.querySelectorAll(".collapsible");
     expandAllButton.addEventListener("click", () => {
-        console.log(expandedAllFlag);
         if (expandedAllFlag) {
             expandedAllFlag = false;
             expandAllButton.textContent = "Collapse all";
@@ -81,6 +38,5 @@ function createExpandAll() {
 
 let expandedAllFlag = false;
 
-populateTOC();
 createCollapses();
 createExpandAll();
