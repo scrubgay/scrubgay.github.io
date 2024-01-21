0ebd40e9478c24eb3b3c3211bb68dc46dfbd7f2b
Renz Torres <104774186+scrubgay@users.noreply.github.com>
2024-01-20 22:31:08 -0500

    three oops

--- myl/methali.js
+++ myl/methali.js
@@ -1,22 +1,15 @@
 const get_methalis = (apiUrl) => {
-    fetch(apiUrl)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Network response was not ok: ${response.statusText}`);
-        }
-        return response.json();
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
+  const resp = fetch(apiUrl);
+  const methalis = resp.json();
+  return methalis;
 }
 
 const get_myl = (methalis) => {
     const today = Math.floor(Date.now()/(1000*60*60*24));
-    const random = new Math.seedrandom(today)
-    const index = Math.floor((methalis.length+1) * random.quick())
+    const random = new Math.seedrandom(today);
+    const index = Math.floor((methalis.length+1) * random.quick());
 
-    return methalis[index]
+    return methalis[index];
 }
 
 const insert_myl = (myl) => {
@@ -45,6 +38,6 @@ const insert_myl = (myl) => {
     anch.appendChild(source);
 }
 
-const methalis = get_methalis("methalis.json")
+const methalis = get_methalis("methalis.json");
 const myl = get_myl(methalis);
 insert_myl(myl);
